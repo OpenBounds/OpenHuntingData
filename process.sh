74d567e571a81165d6651aea5be7ae4429e06ed1
Jesse Crocker <jesse@gaiagps.com>
2016-08-26 15:30:58 -0600

    Deleting existing mbtiles before generating

--- process.sh
+++ process.sh
@@ -6,7 +6,12 @@ DOWNLOAD_CACHE=cache python ./Processing/process.py sources/ $RESULT_DIR 2>&1
 
 echo "Generating vector tiles"
 for i in $RESULT_DIR/*/*/*.geojson; do
-python ./Processing/vectorTiling.py --layer regions `dirname $i`/`basename $i .geojson`.pbf.mbtiles $i
+    MBTILES=`dirname $i`/`basename $i .geojson`.pbf.mbtiles
+    if [ -e $MBTILES ] 
+        then
+            rm $MBTILES
+    fi
+    python ./Processing/vectorTiling.py --layer regions $MBTILES $i
 done
 
 echo "Uploading to s3"
